847f46f65f2e9da23bd956b63441bc8dda2c1341
NafisakhonB <nafisakhonbadalova@gmail.com>
2024-03-10 22:09:02 +0200

    task 2

---
+++ task_2.py
@@ -0,0 +1,18 @@
+#hello  Nafisa, I'm your  assistant.
+print("hello", "Nafisa, I'm your " " assistant", sep= "  ", end= ".\n")
+#Hello
+print("Hello", sep= "")
+#Nafisa
+print("Nafisa", end= ".\n")
+
+
+
+
+print("   ###")
+print(" ##   ##")
+print(" ##   ##")
+print(" ##   ##")
+print("  ####")
+
+print("#     # \n#     # \n####### \n#     # \n#     # ")
+
