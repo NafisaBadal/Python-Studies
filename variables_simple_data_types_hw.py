33bc968d92e9c18e1c007a3e8da1fb768ce57d02
NafisakhonB <nafisakhonbadalova@gmail.com>
2024-03-10 22:23:03 +0200

    variable_simple_data_types_hw.py

---
+++ variables_simple_data_types_hw.py
@@ -0,0 +1,22 @@
+#task_1
+name = input("Print your name:  ",)
+day = input(" What day is it?:  ")
+print(name, "Good day!")
+print(day, "is a perfect day to learn some python")
+
+#task_2
+name = "Nafisa"
+day = "Sunday"
+string_concatenation = "Good Day " + name + "!" + day + " is a perfect day to learn some python."
+print(string_concatenation)
+
+#task_3
+a = 3
+b = 5
+print(a+b)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a**b)
+print(a-b)
+
